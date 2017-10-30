965c63e2bc2a76e14e18bbf6eef198e20b87ed4e
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-10-30 01:22:30 +0100

    style: details-view done

--- client/src/app/details/details.component.ts
+++ client/src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 import { HistoryService } from '../services/history.service';
 import { SwapiService } from '../services/swapi.service';
 
@@ -12,9 +13,15 @@ export class DetailsComponent implements OnInit {
 
   film: object;
   showDetails: boolean = false;
+  characters: Array<object> = [];
+  planets: Array<object> = [];
+  starships: Array<object> = [];
+  vehicles: Array<object> = [];
+  species: Array<object> = [];
 
   constructor(private historyService: HistoryService, 
               private swapi: SwapiService, 
+              private http: Http,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -36,6 +43,50 @@ export class DetailsComponent implements OnInit {
   private pushToHistory() {
     const path = 'details';
     this.historyService.checkCookie(path);
+  }
+
+  private removeClass(elem) {
+    elem.classList.remove('anchor');
+  }
+
+  getChars(characters, elem) {
+    characters.forEach(char => {
+      this.http.get(char)
+      .subscribe(character => this.characters.push(character.json()));
+    });
+    this.removeClass(elem);   
+  }
+  
+  getPlanets(planets, elem) {
+    planets.forEach(planet => {
+      this.http.get(planet)
+      .subscribe(planet => this.planets.push(planet.json()));
+    });
+    this.removeClass(elem);   
+  }
+
+  getStarships(starships, elem) {
+    starships.forEach(starship => {
+      this.http.get(starship)
+      .subscribe(starship => this.starships.push(starship.json()));
+    });
+    this.removeClass(elem);   
+  }
+
+  getVehicles(vehicles, elem) {
+    vehicles.forEach(vehicle => {
+      this.http.get(vehicle)
+      .subscribe(vehicle => this.vehicles.push(vehicle.json()));
+    });
+    this.removeClass(elem);   
+  }
+
+  getSpecies(species, elem) {
+    species.forEach(specie => {
+      this.http.get(specie)
+      .subscribe(specie => this.species.push(specie.json()));
+    });
+    this.removeClass(elem);   
   }
 
 }
